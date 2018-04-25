584baa728f996805b5927a92e47eebb9774463cd
zxliu17 <35967051+zxliu17@users.noreply.github.com>
2018-04-26 00:22:46 +0100

    Simple Combine the Beliefs
    plot the simlarities and c?

--- PythonExercise.py
+++ PythonExercise.py
@@ -4,50 +4,102 @@ Created on Fri Apr  6 16:13:27 2018
 
 @author: liuzx
 """
-
+import matplotlib.pyplot as plt
 import numpy as np
+import random
+import copy
+
 
 def hammingdis(x, y):  
     vec = (x ^ y)
     return sum (vec)  
     
-an = 50 #Number of agents
+an = 100 #Number of agents
 sn = 5  #Number of states
-N = 5000 # Times of iterations 
-agents = np.random.randint(0,2,(an,sn)) #Set every agents to some state randomly
+N = 1000 # Times of iterations 
 
+'''
+initialise the agents randomly
+'''
+single_agent =set()# [set()]*an
+agents_temp = []
+agent_tuple = []
+states = [0]*sn
+agents = []
+# initialise the agents
 for i in range(an):
-    if(sum(agents[i])== 0):
-         print(sum(agents[i]))
-         #print("pre")
-         #print(agents[i])
-         agents[i]= np.random.randint(0,2,(sn))
-         #print("after")
-         #print(agents[i])
-         #print(i)
-         i=i-1
-         #print(i)
-      
-print(agents)
+    for j in range (sn):
+        states[j] = (random.randint(0,1))
+    states_set = tuple(states)
+    agent_tuple.append(states_set)
+    single_agent = set (agent_tuple)
+    #print (states_set)
+    #print (single_agent)    
+    agent_tuple.clear()
+    #print (single_agent)
+    agents.append(single_agent)
 
+#print (agents)
+'''print (len(agents))
+print (agents[0]&agents[1])
+
+print (agents[0]|agents[1])'''
+
+averagesim = [];
 iteration=0 # iteration time count
-
+length = [];
 while iteration < N:
     iteration =iteration +1 
    # print (iteration)
-    index1 = np.random.randint(0,an)
-    index2 = np.random.randint(0,an)
+    index1 = random.randint(0,an-1)
+    index2 = random.randint(0,an-1)
     t = agents [index1]
     s = agents [index2]
-    distance = hammingdis(s,t) # check if overlap exists
-    if (distance == sn) : 
-           agents [index1] =s |t #Union if Hamming distance equals to sn 
-    #    print (agents [index1])
+    Intersection = agents[index1]&agents[index2]
+    Union = agents[index1]|agents[index2]
+    #distance = hammingdis(s,t) # check if overlap exists
+    if (Intersection == set()) : 
+           agents [index1] =Union #Union if Hamming distance equals to sn 
+           agents [index2] =Union
+           #print (agents [index1])
+           #print (1)
     else:
-           agents [index2]= s & t #intersect if not
-     #   print (agents [index2])
-    
-    variance = np.var(agents, axis = 0)
+           agents [index1]=Intersection#intersect if not
+           agents [index2]=Intersection
+           #print (agents [index2])
     #print (variance)
-print(agents)
+#print(agents)
+    sumlength = 0;  
+    for i in range(an):
+        sumlength = sumlength + len (agents[i])
+        
+    length.append(sumlength/an);
+        
+    similarity=[]
+    simtotal = []
+#calculate similarity
+    Num = int(an*(an-1)/2)-1
+    for i in range(an):
+        for j in range (i,an):
+            Num_inter =len(agents[i]&agents[j])
+            Num_uni = len(agents[i]|agents[j])
+            similarity.append((Num_inter/Num_uni))
+        simtotal.append(similarity)
+    averagesim.append(sum(similarity)/len(similarity))
+    #print (Num_inter)
+    #print (Num_uni)
+print (averagesim)  
+plt.plot(averagesim)#, sta)
+#plt.xlabel('Time (ms)')
+#plt.ylabel('Stimulus')
+#plt.title('Spike-Triggered Average')
+#plt.savefig('C:\\Users\\liuzx\\Spyderpro\\CW2\\Computational-Neuroscience-coursework2\\Spike_Triggered_Average(Q3)')
+plt.show()
+plt.plot(length)
+plt.show()
+#print (similarity)
+#print (simtotal)
+
+
+
 
