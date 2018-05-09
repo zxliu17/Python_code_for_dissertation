5e4731fe6598dc004780cf424d8b7583b5ef9688
zxliu17 <35967051+zxliu17@users.noreply.github.com>
2018-05-09 04:19:20 +0100

    code with Histogram

--- Code_histogram.py
+++ Code_histogram.py
@@ -9,10 +9,80 @@ import numpy as np
 import random
 import copy
 import math
+import time
+import pickle
 '''Function for hamming distance'''
 def hammingdis(x, y):  
     vec = (x ^ y)
     return sum (vec)  #return value is a scalar
+def create_world(propsition_number):
+    single_agent =set()# [set()]*an # use SET as for any agents
+   # agents_temp = []
+    agent_tuple = [] # belief as tuple(tuple is unchangeable)
+    states = [0]*propsition_number #proposition as list 
+    world = [] #A list of sets of tuple
+    # initialise the agents
+    agents_number = int(math.pow(2,propsition_number))
+    for i in range(agents_number):
+        basic = i+1
+        for j in range (propsition_number):
+            states[j] = basic%2
+            basic = basic//2
+        states_set = tuple(states)
+        agent_tuple.append(states_set)
+        single_agent = set (agent_tuple)
+        #print (states_set)
+        #print (single_agent)    
+        agent_tuple.clear()
+    #print (single_agent)
+        world.append(single_agent)
+#    world_set = world[0]
+#    for k in range(agents_number):
+#        world_set = world_set|world[k]
+
+    return world
+    
+
+def random_initialise(agents_number, propsition_number,world):
+    '''
+    initialise the agents randomly (with a random number of beliefs)
+    '''
+    single_agent =set()# [set()]*an # use SET as for any agents
+   # agents_temp = []
+    agent_tuple = [] # belief as tuple(tuple is unchangeable)
+    states = [0]*propsition_number #proposition as list 
+    agents = [] #A list of sets of tuple
+    indexlist = list(range(2**propsition_number))
+#    print(indexlist)
+    # initialise the agents
+    for i in range(agents_number):
+        for j in range (propsition_number):
+            states[j] = (random.randint(0,1))
+        states_set = tuple(states)
+        agent_tuple.append(states_set)
+        single_agent = set (agent_tuple)
+        #print (states_set)
+        #print (single_agent)    
+        agent_tuple.clear()
+    #print (single_agent)
+        agents.append(single_agent)
+    for num in range(len(agents)):
+        num_blf = random.randint(1,int(math.pow(2,propsition_number)))
+        agt = [agents[num]]
+        random.shuffle(indexlist)
+        k = 0
+        while cal_card(agt) < num_blf :
+#            print (agt)
+#            print(cal_card(agt))
+#            print (num_blf)
+#            print(agents)
+            agents[num] = agents[num]|world[indexlist[k]]
+            k=k+1
+            agt = [agents[num]]
+   
+            
+    return agents
+
 
 def initialise_agents(agents_number, propsition_number):
     '''
@@ -119,11 +189,20 @@ def iteration(agents,agent_number, iteration_times):
     #print (simtotal)
     '''
     return averagesim, cardinality , agents
+
+def text_save(content,filename,mode='a'):
+    # Try to save a list variable in txt file.
+    file = open(filename,mode)
+    for i in range(len(content)):
+        file.write(str(content[i])+'\n')
+    file.close()
+
+start = time.clock()
     
 an = 50 #Number of agents
-sn = 3  #Number of propsitions
-N = 500 # Times of iterations 
-T = 300
+sn = 4  #Number of propsitions
+N = 1000 # Times of iterations 
+T = 800
 sim = []
 card = []
 AVEsim=[]
@@ -134,7 +213,10 @@ convergePos = []
 #agents = initialise_agents(an, sn);
 
 for i in range (T):
-    agents = initialise_agents(an, sn);
+    '''when change the initialise method,
+    Remember to change the FILENAME and FIGURENAME'''
+#    agents = initialise_agents(an, sn)
+    agents = random_initialise(an, sn,create_world(sn))
     trans = copy.deepcopy(agents)
     #print (agents)
     (averagesim, cardinality, store) = iteration(trans,an,N)
@@ -173,26 +255,54 @@ while j < len(stdsim):
     AVEcard_f.append(AVEcard[j])
     AVEsim_f.append(AVEsim[j])
     j = j+50
-    
-
+elapsed = (time.clock() - start)
+print("Time used:",elapsed)  
+filename ='data'+str(an)+'_'+str(sn)+'_'+str(N)+'_'+str(T)
+figurename = str(an)+'_'+str(sn)+'_'+str(N)+'_'+str(T)
 
 plt.figure(1)
 plt.plot(AVEsim)
+plt.xlabel('Similarity')
+plt.ylabel('Iterations')
+plt.title('Similarity-Iteration')
+#plt.legend()
+plt.savefig('Sim'+figurename+'.png',dpi = 600)
 plt.show()
 plt.figure(2)
 plt.plot(AVEsim,color = 'brown')
 plt.errorbar(index, AVEsim_f, yerr = stdsim_f, fmt ='o',color = 'brown')
+plt.xlabel('Similarity')
+plt.ylabel('Iterations')
+plt.title('Similarity-Iteration with errorbar')
+#plt.legend()
+plt.savefig('SimErr'+figurename+'.png',dpi = 600)
 plt.show()
 plt.figure(3)
 plt.plot(AVEcard)
+plt.xlabel('Cardinality')
+plt.ylabel('Iterations')
+plt.title('Cardinality-Iteration')
+#plt.legend()
+plt.savefig('Card'+figurename+'.png',dpi = 600)
 plt.show()
 plt.figure(4)
 plt.plot(AVEcard,color = 'brown')
 plt.errorbar(index, AVEcard_f, yerr = stdcard_f, fmt ='o',color = 'brown')
+plt.xlabel('Cardinality')
+plt.ylabel('Iterations')
+plt.title('Cardinality-Iteration with errorbar')
+#plt.legend()
+plt.savefig('CardErr'+figurename+'.png',dpi = 600)
 plt.show()
 plt.figure(5)
 plt.bar(xaxis,countagt,color = 'black',width = 0.4) 
+plt.xlabel('Agents Number')
+plt.ylabel('Times')
 plt.title("Times of covergence") 
+plt.savefig('agt'+figurename+'.png',dpi = 600)
 plt.show()
-
+text_save([AVEsim, AVEcard, stdsim_f,stdcard_f,countagt,elapsed],filename,mode='a')
+f= open(filename, 'wb')
+pickle.dump([AVEsim, AVEcard, stdsim_f,stdcard_f,countagt,elapsed], f)
+f.close()
 
